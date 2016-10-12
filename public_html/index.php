72ac0b5a77574d68aeb540e3e5bb51c4ae832527
Ana Vela <avela7@cnm.edu>
2016-10-12 13:35:50 -0600

    Added profile, use case, and entitities

--- public_html/index.php
+++ public_html/index.php
@@ -1,17 +1,54 @@
 <!DOCTYPE html>
-<html lang="em">
+	<html lang="en">
 	<head>
-		<meta charset="UTF-8>
-		<title>Data Design Assignment - Instagram</title>
-
+		<meta charset="UTF-8">
+		<title>Data Design Assignment - Instagram Profile and Post</title>
 	</head>
 
 	<body>
+	<h1>Data Design Assignment</h1>
+		<h2>Instagram Profile and Post</h2>
+
+		<!-- start persona section -->
+		<section>
+			<h3>Persona</h3>
+			<p>Name: McKenna Shutterfly</p>
+			<p>Age: 26</p>
+			<p>Profession: McKenna is a new freelance photographer in Albuquerque. She wants to share the photographs she has taken around the city and highlight her best photos from various gigs.</p>
+			<p>Technology: McKenna invested in professional equipment for photography. She also uses her iPhone for photos when she sees a great photo opportunity and she doesn't have her professional equipment with her.</p>
+			<p>Attitudes and Behaviors: McKenna is still working full-time in retail, so she can only dedicate time to her photography when she is not working. She hopes to transition to photography full-time when she gets more consistent gigs.</p>
+			<p>Frustations and Needs: She does not have a website yet, so Instagram is one of the various ways she is posting her work via social media to find more clients. She has a limited time to post photos, so she needs a quick way to get her work online.</p>
+			<p>Goal: McKenna wants to post photos regularly, so she needs to be able to post photos easily. She also wants to add tags to the post quickly as well. Once she gets a website, she wants to add a link to her Instagram page so that potential clients can directed to her official page.</p>
+		</section>
+
+		<!-- Start use case section -->
+		<section>
+			<h3>Use Case</h3>
+			<p> McKenna recently created flyers and business cards to promote her photography work. She hopes that potential clients visit her Instagram page to be able to preview her work and eventually hire her for an event or photo session.</p>
+			<p>A few of McKenna's friends offered to post flyers and give out business cards for her. She wants to be ready with good examples of her work once more people beginning checking out her Instagram page.</p>
+		</section>
+		<!-- Start entities section -->
+
+		<section>
+			<h3>Profile and Post Entities</h3>
+			<ol>
+				<li>Profile Name</li>
+				<li>Email Address</li>
+				<li>Profile Description</li>
+				<li>Profile Photo</li>
+				<li>Location</li>
+				<li>Follow Feature</li>
+				<li>Post Photos</li>
+				<li>Post Tags</li>
+				<li>Post Likes</li>
+				<li>Post Comments</li>
+				<li>Post Shares</li>
+				<li></li>
+
+			</ol>
+
+
+
 
 	</body>
-
-
-
-
-
-</html>
+	</html>
