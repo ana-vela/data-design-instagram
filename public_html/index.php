de875a60466db3f366bd874c7dafb6c3f6219890
Ana Vela <avela7@cnm.edu>
2016-10-15 11:34:02 -0600

    updated css file, added two erd images

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,7 @@
 	<html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<!-- <link href="css/stylesheet.css" rel="stylesheet" type="text/css"> -->
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css">
 			<title>Data Design Assignment - Instagram Profile and Post</title>
 	</head>
 
@@ -13,13 +13,13 @@
 				<!-- start persona section -->
 				<section>
 				<h3>Persona</h3>
-					<p>Name: McKenna Shutterfly</p>
-					<p>Age: 26</p>
-					<p>Profession: McKenna is a new freelance photographer in Albuquerque. She wants to share the photographs she has taken around the city and highlight her best photos from various gigs.</p>
-					<p>Technology: McKenna invested in professional equipment for photography. She also uses her iPhone for photos when she sees a great photo opportunity and she doesn't have her professional equipment with her. It is much easier for her to post photos from her iPhone, so she chooses to upload from her iPhone more often than her regular camera.</p>
-					<p>Attitudes and Behaviors: McKenna is still working full-time in retail, so she can only dedicate time to her photography when she is not working. She hopes to transition to photography full-time when she gets more consistent gigs.</p>
-					<p>Frustations and Needs: She does not have a website yet, so Instagram is one of the various ways she is posting her work via social media to find more clients. She has a limited time to post photos, so she needs a quick way to get her work online.</p>
-					<p>Goal: McKenna wants to post several of her most recent photos on Instagram. Since she is short on time, she will post photos from her iPhone. Since she loves Albuquerque, she wants to share many of the photos she has taken around town. This may help her create a sense of community with potential clients. Once she gets a website, she wants to add a link to her Instagram page so that potential clients can be directed to her official page.</p>
+					<p><strong>Name:</strong> McKenna Shutterfly</p>
+					<p><strong>Age:</strong> 26</p>
+					<p><strong>Profession:</strong> McKenna is a new freelance photographer in Albuquerque. She wants to share the photographs she has taken around the city and highlight her best photos from various gigs.</p>
+					<p><strong>Technology:</strong> McKenna invested in professional equipment for photography. She also uses her iPhone for photos when she sees a great photo opportunity and she doesn't have her professional equipment with her. It is much easier for her to post photos from her iPhone, so she chooses to upload from her iPhone more often than her regular camera.</p>
+					<p><strong>Attitudes and Behaviors:</strong> McKenna is still working full-time in retail, so she can only dedicate time to her photography when she is not working. She hopes to transition to photography full-time when she gets more consistent gigs.</p>
+					<p><strong>Frustations and Needs:</strong> She does not have a website yet, so Instagram is one of the various ways she is posting her work via social media to find more clients. She has a limited time to post photos, so she needs a quick way to get her work online.</p>
+					<p><strong>Goal:</strong> McKenna wants to post several of her most recent photos on Instagram. Since she is short on time, she will post photos from her iPhone. Since she loves Albuquerque, she wants to share many of the photos she has taken around town. This may help her create a sense of community with potential clients. Once she gets a website, she wants to add a link to her Instagram page so that potential clients can be directed to her official page.</p>
 				</section>
 
 				<!-- Start use case section -->
@@ -99,5 +99,7 @@
 									</ul>
 
 				</section>
+
+		<!-- <img src="img/data-design-instagram-erd.pdf" alt="ERD Chart"> -->
 		</body>
 	</html>
